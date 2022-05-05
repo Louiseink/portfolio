edf527fb6a7276bee05d4be3aec903869fcc330f
Slabstick <slabsticktwitch@gmail.com>
2022-05-05 15:51:47 +0200

    Work on responsiveness for mobile page

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ const scrollline = document.querySelector('.scroll-line');
       };
 
       window.addEventListener('scroll', fillscrollline);
+
+let menu = document.querySelector('.smallmenu');
+let menuButton = document.querySelector('.menuicon');
+
+// menu.style.top = '30px';
+menu.style.opacity = 0;
+menu.style.zIndex = 100;
+
+function showMenu() {
+  let menuOpac = parseInt(menu.style.opacity);
+  if (menuOpac < 100) {
+    menu.style.transition = 'all .3s ease-in';
+    menu.style.opacity = 100;
+    
+  } else {
+    menu.style.transition = 'all .3s ease-in';
+    menu.style.opacity = 0;
+  }
+}
+
+function hideMenu() {
+  let menuOpac = parseInt(menu.style.opacity);
+  if (menuOpac > 1) {
+    menu.style.transition = 'all .3s ease-in';
+    menu.style.opacity = 0;
+  }
+}
+
+menuButton.addEventListener('click', showMenu);
+document.addEventListener('mousedown', hideMenu);
